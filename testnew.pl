c76e8ada5b3789cdb097ee62dcba553e942e551b
hjonline <hj_182006@hotmail.com>
2014-04-07 12:07:48 +0800

    should be finished

--- testnew.pl
+++ testnew.pl
@@ -7,7 +7,9 @@ use Encode;
 # 定义日期 
 #{{{
 my $base_workweek=35;
-my $base_workyear="2013W";
+my $base_year = "2013";
+my $base_workyear=$base_year . "W";
+my @short_holidays = qw (0101 0405 0406 0407 0501 0502 0503 0602 0908 1001 1002 1003 1004 1005 1006 1007 );
 #}}}
 my @classes_teachers;
 #{{{
@@ -123,17 +125,95 @@ my @classes_lessons;
 
 my @weeks_grades_subjects;
 #{{{
-# gawk "{print \"[\"$1\"],\",\"[\"$2\"],\",\"[\"$3\"]\"}" testlesson1.txt > t2.txt
+# gawk "{print \"[\"$1\"],\",\"[\"上\"],\",\"[\"$2\"],\",\"[\"$3\"]\"}" testlesson1.txt > t2.txt
 # replace [ ] with " " in t2.txt
 # gawk "{print \"[\"$0\"],\"}" t2.txt > t3.txt
 @weeks_grades_subjects = (
-			  ["第一周", "一", "第一课"],
-			  ["第一周", "二", "第二课"],
-			  ["第二周", "三", "第三课"],
-			  ["第三周", "四", "第四课"],
-			  ["第四周", "五", "第五课"],
+			  ["第一周", "上", "一", "第一课"],
+			  ["第一周", "上", "二", "第二课"],
+			  ["第二周", "上", "三", "第三课"],
+			  ["第三周", "上", "四", "第四课"],
+			  ["第六周", "上", "五", "第五课"],
 );
+#}}}
+my %week_number;
+#{{{
+%week_number = (
+		   "第一周" => 0,
+		   "第二周" => 1,
+		   "第三周" => 2,
+		   "第四周" => 3,
+		   "第五周" => 4,
+		   "第六周" => 5,
+		   "第七周" => 6,
+		   "第八周" => 7,
+		   "第九周" => 8,
+		   "第十周" => 9,
+		   "第十一周" => 10,
+		   "第十二周" => 11,
+		   "第十三周" => 12,
+		   "第十四周" => 13,
+		   "第十五周" => 14,
+		   "第十六周" => 15,
+		   "第十七周" => 16,
+		   "第十八周" => 17,
+);
+#}}}
+my %half_week;
+#{{{
+%half_week = (
+	      "上" => 0,
+	      "下" => 1,
+);
+#}}}
+my %grade_number;
+#{{{
+%grade_number = (
+		 "一" => 0,
+		 "二" => 1,
+		 "三" => 2,
+		 "四" => 3,
+		 "五" => 4,
+);
+#}}}
+my @holidays;
+#{{{
+@holidays = @short_holidays;
+foreach my $hdays (@holidays) {
+  $hdays = $base_year . $hdays;
+};
 #}}}
 #}}}
 
-print $classes_lessons[0][0]{"一（5）"}. "\n";
+my $cur_week;
+my $cur_day;
+my $cur_time;
+my $t_date;
+my $cur_teacher;
+my $cur_class;
+my $cur_subject;
+my $in_hdays;
+foreach my $emt_line (@weeks_grades_subjects) {
+  $cur_week = $base_workweek + $week_number{@$emt_line[0]};
+  while (($key,$value) = each $classes_lessons[@$emt_line[1]][$grade_number{@$emt_line[2]}] ) {
+    $in_hdays = 0;
+    $cur_day = $value;
+    $cur_time = $base_workyear . $cur_week . $cur_day; 
+    $t_date = ParseDateString($cur_time);
+    $t_date = UnixDate($t_date,"%Y%m%d");
+    # 跳过假期
+    foreach my $hdays (@holidays) {
+      if ($hdays == $t_date) {
+	$in_hdays = 1;
+	last;
+      }
+    };
+    if ($in_hdays == 1) {
+      next;
+    } else {
+      print "$t_date $key ";
+      print "$classes_teachers[$grade_number{@$emt_line[2]}]{$key} @$emt_line[3]\n";
+    }
+  }
+
+}; 
